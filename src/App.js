50500cc277d4a7f17a4da961320c6105add08f25
guramikvakhadze <gkvaxadze@gmail.com>
2023-08-07 19:41:41 +0100

    add change page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import frontcard from "./image/bg-card-front.png";
 import backcard from "./image/bg-card-back.png";
 import cardlogo from "./image/card-logo.svg";
 import backgrounimage from "./image/bg-main-desktop.png";
+import { useState } from "react";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
           <img className="frontcard" src={frontcard} />
           <div className="frontcardtext">
             <img className="cardlogo" src={cardlogo} />
-            <p className="cardnumber1">0000 0000 0000 0000 0000</p>
+            <p className="cardnumber1">0000 0000 0000 0000</p>
             <div className="nameanddate">
               <p className="name1">JANE APPLESEED</p>
               <p className="date1">00/00</p>
--- src/Confirm.js
+++ src/Confirm.js
@@ -1,7 +1,9 @@
 import "./Confirm.css";
+import Continue from "./Continue";
 import React, { useState } from "react";
 
 function Card() {
+  const [showcard, setShowcard] = useState(false);
   const [name, setName] = useState("");
   const [nameVerified, setNameverified] = useState(null);
 
@@ -13,7 +15,14 @@ function Card() {
   const [number, setNumber] = useState("");
   const [numberVerified, setNumberverified] = useState(null);
 
-  const handleNumberCange = (e) => {
+  const handleNumberChange = async (e) => {
+    if (e.target.value.length === 4) {
+      e.target.value += " ";
+    } else if (e.target.value.length === 9) {
+      e.target.value += " ";
+    } else if (e.target.value.length === 14) {
+      e.target.value += " ";
+    }
     setNumber(e.target.value);
     setNumberverified(null);
   };
@@ -44,10 +53,10 @@ function Card() {
 
   const handleConfirmClick = () => {
     const cvcRegex = /^\d{3}$/;
+    const isCvcValid = cvcRegex.test(cvc);
     if (cvc === "") {
       setCvcVerified("");
     } else {
-      const isCvcValid = cvcRegex.test(cvc);
       setCvcVerified(isCvcValid);
     }
 
@@ -55,17 +64,27 @@ function Card() {
     const isNmaevalid = nameRegex.test(name);
     setNameverified(isNmaevalid);
 
-    const numberRegex = /^[0-9]{20}$/;
+    const numberRegex = /^(?:\d{4}\s){3}\d{4}$/;
     const isNumbervalid = numberRegex.test(number);
     setNumberverified(isNumbervalid);
 
-    const yearRegex = /^\d{4}$/;
+    const yearRegex = /^(?:2[3-9]|[3-9]\d)$/;
     const isYearValid = yearRegex.test(year);
     setYearverified(isYearValid);
 
     const dateRegex = /^(0?[1-9]|1[0-2])$/;
     const isDateValid = dateRegex.test(date);
     setDateVerified(isDateValid);
+
+    if (
+      isNmaevalid &&
+      isNumbervalid &&
+      isDateValid &&
+      isCvcValid &&
+      isYearValid
+    ) {
+      setShowcard(true);
+    }
   };
 
   const nameinputstyle = {
@@ -103,82 +122,86 @@ function Card() {
   };
 
   return (
-    <div className="inputdiv">
-      <div className="cardholderdiv">
-        <p className="cardholder">CARDHOLDER NAME</p>
-        <input
-          onChange={handleNameChange}
-          placeholder="e.g. Jane Appleseed"
-          type="text"
-          className="inputname"
-          style={nameinputstyle}
-        ></input>
-      </div>
-      <div className="cardnumberdiv">
-        <p className="cardnumber">CARD NUMBER</p>
-        <input
-          onChange={handleNumberCange}
-          placeholder="e.g. 1234 5678 9123 0000"
-          tupe="number"
-          className="inputnumber"
-          style={numberinputstyle}
-        ></input>
-        {numberVerified === false && (
-          <p className="errornumber" style={{ color: "red" }}>
-            Wrong format, numbers only
-          </p>
-        )}
-      </div>
-      <div className="carddatediv">
-        <p className="carddate">EXP. DATE (MM.YY) CVC</p>
-        <div className="datediv">
+    <div>
+      {showcard === true && <Continue />}
+      <div className={showcard ? "inputdiv remove" : "inputdiv"}>
+        <div className="cardholderdiv">
+          <p className="cardholder">CARDHOLDER NAME</p>
           <input
-            type="number"
-            onChange={handleDateChange}
-            placeholder="MM"
-            className="mm"
-            style={dateinputstyle}
-          ></input>
-          <input
-            onChange={handleYearChange}
-            placeholder="YY"
-            className="yy"
-            style={yearinputstyle}
-          ></input>
-          <input
-            onChange={handleCvcChange}
-            placeholder="cvc"
-            className="cvc1"
-            style={cvcinputstyle}
+            onChange={handleNameChange}
+            placeholder="e.g. Jane Appleseed"
+            className="inputname"
+            style={nameinputstyle}
           ></input>
         </div>
-      </div>
-      <div className="errordatecvc">
-        {dateVerified === false && (
-          <p className="errordate" style={{ color: "red" }}>
-            Can’t be blank
-          </p>
-        )}
-        {yearVerified === false && (
-          <p className="errordate" style={{ color: "red" }}>
-            Can’t be blank
-          </p>
-        )}
+        <div className="cardnumberdiv">
+          <p className="cardnumber">CARD NUMBER</p>
+          <input
+            type="text"
+            onChange={handleNumberChange}
+            placeholder="e.g. 1234 5678 9123 0000"
+            className="inputnumber"
+            style={numberinputstyle}
+          ></input>
+          {numberVerified === false && (
+            <p className="errornumber" style={{ color: "red" }}>
+              Wrong format, numbers only
+            </p>
+          )}
+        </div>
+        <div className="carddatediv">
+          <p className="carddate">EXP. DATE (MM.YY) CVC</p>
+          <div className="datediv">
+            <input
+              type="number"
+              onChange={handleDateChange}
+              placeholder="MM"
+              className="mm"
+              style={dateinputstyle}
+            ></input>
+            <input
+              type="number"
+              onChange={handleYearChange}
+              placeholder="YY"
+              className="yy"
+              style={yearinputstyle}
+            ></input>
+            <input
+              type="number"
+              onChange={handleCvcChange}
+              placeholder="cvc"
+              className="cvc1"
+              style={cvcinputstyle}
+            ></input>
+          </div>
+        </div>
+        <div className="errordatecvc">
+          {dateVerified === false && (
+            <p className="errordate" style={{ color: "red" }}>
+              Can’t be blank
+            </p>
+          )}
+          {yearVerified === false && (
+            <p className="errordate" style={{ color: "red" }}>
+              Can’t be blank
+            </p>
+          )}
 
-        {cvcVerified === "" && (
-          <p className="errorcvc" style={{ color: "red" }}>
-            Can’t be blank
-          </p>
-        )}
-        {cvcVerified === false && (
-          <p className="errorcvc" style={{ color: "red" }}>
-            3 numbers only
-          </p>
-        )}
+          {cvcVerified === "" && (
+            <p className="errorcvc" style={{ color: "red" }}>
+              Can’t be blank
+            </p>
+          )}
+          {cvcVerified === false && (
+            <p className="errorcvc" style={{ color: "red" }}>
+              3 numbers only
+            </p>
+          )}
+        </div>
+        <button onClick={handleConfirmClick} className="button">
+          Confirm
+        </button>
       </div>
-      <button onClick={handleConfirmClick} className="button">
-        Confirm
-      </button>
     </div>
   );
 }
